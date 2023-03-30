ee6c4a4ec2a0285ab0d12618aa5d3389417d5e9a
rebontadeb <rebontadeb@gmail.com>
2023-03-30 15:45:04 +0530

    sample_webserver - healthz

--- sample_webserver/routes/health.go
+++ sample_webserver/routes/health.go
@@ -19,5 +19,6 @@ func Healtz(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonResponse)
+		selflogger.InfoLogger.Println("Healthz Endpoint Accessed")
 	}
 }
